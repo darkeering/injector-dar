c0c9654acb89735de87c33df444985f194ea7699
darkeering <darkeering@outlook.com>
2022-12-20 15:54:39 +0800

    ddd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "reflect-metadata";
 
 function App() {
   return (
@@ -24,3 +25,28 @@ function App() {
 }
 
 export default App;
+
+
+const Injectable1 = (): ClassDecorator => (target) => {};
+
+class OtherService {
+  a = 1;
+}
+
+@Injectable1()
+class TestService {
+  constructor(public readonly otherService: OtherService) {}
+
+  testMethod() {
+    console.log(this.otherService.a);
+  }
+}
+
+const Factory1 = (target: any) => {
+  // 获取所有注入的服务
+  const providers = Reflect.getMetadata("design:paramtypes", target); // [OtherService]
+  const args = providers.map((provider: any) => new provider());
+  return new target(...args);
+};
+
+Factory1(TestService).testMethod(); // 1
